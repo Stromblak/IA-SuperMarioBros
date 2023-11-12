e550aa7c68f49051eeff090c8d0e04da695c0799
dazfz <dazhi891@gmail.com>
2023-11-12 16:05:24 -0300

    ppo y ambiente para correrlo

---
+++ ppo/mario.py
@@ -0,0 +1,19 @@
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym.wrappers import GrayScaleObservation
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+from stable_baselines3 import PPO
+
+env = gym_super_mario_bros.make("SuperMarioBros-v0")
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+env = GrayScaleObservation(env, keep_dim=True)
+env = DummyVecEnv([lambda: env])
+env = VecFrameStack(env, 4, channels_order="last")
+
+model = PPO.load("./ppo/train/best_model_5000")
+state = env.reset()
+while True:
+    action, _ = model.predict(state)
+    state, reward, done, info = env.step(action)
+    env.render()
